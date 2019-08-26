898adeb9b8f0dfddf6b82d21799b4a70b2510771
Ruan Aragao <aragao.ruan@gmail.com>
2019-08-26 20:49:02 -0300

    configurando models e correcao migration

--- src/database/migrations/20190826010318-create_users.js
+++ src/database/migrations/20190826010318-create_users.js
@@ -22,7 +22,7 @@ module.exports = {
       },
       provider: {
         type: Sequelize.BOOLEAN,
-        defauldValue: false,
+        defaultValue: false,
         allowNull: false,
       },
       created_at: {
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const router = new Router();
 
-router.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+router.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Ruan Aragao',
+    email: 'aragao.ruan@gmail.com',
+    password_hash: '12345',
+  });
+  return res.json(user);
 });
 
 export default router;
