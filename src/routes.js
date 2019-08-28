64739d537a7850c3a02c3f6e57adb3c2edd54838
Ruan Aragao <aragao.ruan@gmail.com>
2019-08-27 21:31:04 -0300

    [Add] Middleware jwt

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,16 @@ import { Router } from 'express';
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
 
+/** Middlewares */
+import authMiddleware from './app/middlewares/auth';
+
 const router = new Router();
 
 router.post('/users', UserController.store);
 router.post('/sessions', SessionController.store);
+
+router.use(authMiddleware);
+
+router.put('/users', UserController.update);
 
 export default router;
