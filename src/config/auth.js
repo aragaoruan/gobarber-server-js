4250e3181f8656ad48ec3858aa1513bbdbb1eb4d
Ruan Aragao <aragao.ruan@gmail.com>
2019-08-27 21:00:01 -0300

    [Add] JWT intall

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'bdc96cd0f4a35b4c00abdff66f36539c',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ import { Router } from 'express';
 
 /** Controlles */
 import UserController from './app/controller/UserController';
+import SessionController from './app/controller/SessionController';
 
 const router = new Router();
 
 router.post('/users', UserController.store);
+router.post('/sessions', SessionController.store);
 
 export default router;
