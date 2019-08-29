b5508adb6eec9ed95331971044762bae4bbb6fc1
Ruan Aragao <aragao.ruan@gmail.com>
2019-08-29 19:53:42 -0300

    [Add] Update user

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -1,3 +1,4 @@
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 import User from '../models/User';
 
 class UserController {
@@ -18,8 +19,33 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
-    return res.json({ ok: true });
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPasssword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+    // return res.json({ ok: true });
   }
 }
 
