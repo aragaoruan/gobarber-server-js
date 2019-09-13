6a8d64a64c85d851b383dcf155950f4ebf0ffcc1
Ruan Aragao <aragao.ruan@gmail.com>
2019-09-12 21:43:54 -0300

    [Add] Envio de e-mail

---
+++ src/config/mail.js
@@ -0,0 +1,22 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '978b755e75eece',
+    pass: '347fbe5ec614e7',
+  },
+
+  defauld: {
+    from: `Equipe GoBarber <noreply@gobarber.com>`,
+  },
+};
+
+/**
+ * Amazon Sas
+ * Mailgun
+ * Sparkpost
+ * mandril (Mailchimp)
+ *
+ * Mailtrap (DEV)
+ */
