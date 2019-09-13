ef33c252962a666b9210972d7517475183d3341a
Ruan Aragao <aragao.ruan@gmail.com>
2019-09-12 22:22:04 -0300

    [Add] configurando layout para envio de e-mail

--- src/app/controller/AppointmentController.js
+++ src/app/controller/AppointmentController.js
@@ -132,6 +132,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -156,7 +161,15 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Voce tem um novo cancelamento',
+      // text: 'Voce tem um novo cancelamento',
+      template: 'cancelation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', as' H:mm'h ' ", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
