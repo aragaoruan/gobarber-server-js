a78e835a7653b13d8f2a9dc189c41c6bc4ab07f4
Ruan Aragao <aragao.ruan@gmail.com>
2019-09-18 11:49:24 -0300

    [Add] Listando horarios disponiveis

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import ProviderController from './app/controller/ProviderController';
 import AppointmentController from './app/controller/AppointmentController';
 import ScheduleController from './app/controller/ScheduleController';
 import NotificationsController from './app/controller/NotificationsController';
+import AvailableController from './app/controller/AvailableController';
 
 /** Middlewares */
 import authMiddleware from './app/middlewares/auth';
@@ -25,6 +26,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.delete('/appointments/:id', AppointmentController.delete);
